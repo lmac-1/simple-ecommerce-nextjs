8cf82aeb2b15dcfe6d65050f3c5dc0303c851402
lmac-1 <macartney.lucy@gmail.com>
2023-02-20 12:19:47 -0500

    updates title of website

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Basic Next.js ecommerce</title>
+        <title>fresh</title>
         <meta
           name="description"
           content="A simple website to show how to use use-shopping-cart"
