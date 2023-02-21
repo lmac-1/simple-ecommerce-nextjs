949fe3978aa3373cb26937b06e7b2764b4c8fd5f
lmac-1 <macartney.lucy@gmail.com>
2023-02-21 11:25:41 -0500

    Adds extra logic to CheckoutButton

--- components/CheckoutButton.js
+++ components/CheckoutButton.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 export default function CheckoutButton() {
+  const [status, setStatus] = useState("idle");
+
   return (
-    <button className="bg-emerald-50 mt-3 hover:bg-emerald-500 hover:text-white transition-colors duration-500 text-emerald-500 py-3 px-5 rounded-md w-100">
-      Proceed to checkout
-    </button>
+    <article className="mt-3 flex flex-col">
+      {/* <div className="text-red-700 text-xs mb-3 h-5 text-center">
+        Unable to connect to Stripe Checkout
+      </div> */}
+      <button className="bg-emerald-50 hover:bg-emerald-500 hover:text-white transition-colors duration-500 text-emerald-500 py-3 px-5 rounded-md w-100">
+        {status !== "loading" ? "Proceed to checkout" : "Loading..."}
+      </button>
+    </article>
   );
 }
