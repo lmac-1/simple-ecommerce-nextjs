2ca4f2063e3decd59cb469b67ed75acf64925fdb
lmac-1 <macartney.lucy@gmail.com>
2023-02-23 11:40:30 -0500

    test commit

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
         <title>fresh</title>
         <meta
           name="description"
-          content="A simple website to show how to use use-shopping-cart."
+          content="A simple website to show how to use use-shopping-cart"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
